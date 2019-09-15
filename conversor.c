fe6863e9eda1dda90731b9cec32490651a837e03
Luan Oliveira <luanhigor98@gmail.com>
2019-09-15 20:41:20 -0300

    Second commit

--- conversor.c
+++ conversor.c
@@ -1,76 +1,51 @@
+/* 01010111  01110010  01101001  01110100  01110100  01100101  01101110  01100010 
+ * 01111001  01001100  01110101  01100001  01101110  01001000  01010010  01001111 
+ * 01101100  01101001  01110110  01100101  01101001  01110010  01100001  00101110
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 #include "funcoes.h"
 
 /*VARIÁVEIS GLOBAIS*/
 int bin[7];
 int num;
-int caracter;
+char caracter;
+
+void abertura(){
+
+	printf("################################################################\n");
+	printf("###                                                          ###\n");
+	printf("###                   caracter para binário                  ###\n");
+	printf("###                                                          ###\n");
+	printf("################################################################\n");
+
+}
+
+void menu(){
+
+	int menu;
+
+	abertura();
+
+	printf("\nPara nova conversão pressione 1, para sair pressione 0: ");
+	scanf("%i", &menu);
+
+	switch(menu){
+		case 1: dec_for_bin();
+		case 0: exit(1);
+	}
+}
 
 int char_for_int(){
 	/*TRANSFORMA LETRA EM DECIMAL*/
-	caracter = 0;
-	num = 0;
+	scanf(" %c", &caracter);
+	num = caracter;
 
-	printf("Informe uma letra para converte-la em número binário: ");
-	caracter = getchar();
-
-	switch(caracter){
-		/*MAIÚSCULAS*/
-		case 'A': caracter = 65; num = caracter; break;
-		case 'B': caracter = 66; num = caracter; break;
-		case 'C': caracter = 67; num = caracter; break;
-		case 'D': caracter = 68; num = caracter; break;
-		case 'E': caracter = 69; num = caracter; break;
-		case 'F': caracter = 70; num = caracter; break;
-		case 'G': caracter = 71; num = caracter; break;
-		case 'H': caracter = 72; num = caracter; break;
-		case 'I': caracter = 73; num = caracter; break;
-		case 'J': caracter = 74; num = caracter; break;
-		case 'K': caracter = 75; num = caracter; break;
-		case 'L': caracter = 76; num = caracter; break;
-		case 'M': caracter = 77; num = caracter; break;
-		case 'N': caracter = 78; num = caracter; break;
-		case 'O': caracter = 79; num = caracter; break;
-		case 'P': caracter = 80; num = caracter; break;
-		case 'Q': caracter = 81; num = caracter; break;
-		case 'R': caracter = 82; num = caracter; break;
-		case 'S': caracter = 83; num = caracter; break;
-		case 'T': caracter = 84; num = caracter; break;
-		case 'U': caracter = 85; num = caracter; break;
-		case 'V': caracter = 86; num = caracter; break;
-		case 'W': caracter = 87; num = caracter; break;
-		case 'X': caracter = 88; num = caracter; break;
-		case 'Y': caracter = 89; num = caracter; break;
-		case 'Z': caracter = 90; num = caracter; break;
-		/*MINÚSCULAS*/
-		case 'a': caracter = 97; num = caracter; break;
-		case 'b': caracter = 98; num = caracter; break;
-		case 'c': caracter = 99; num = caracter; break;
-		case 'd': caracter = 100; num = caracter; break;
-		case 'e': caracter = 101; num = caracter; break;
-		case 'f': caracter = 102; num = caracter; break;
-		case 'g': caracter = 103; num = caracter; break;
-		case 'h': caracter = 104; num = caracter; break;
-		case 'i': caracter = 105; num = caracter; break;
-		case 'j': caracter = 106; num = caracter; break;
-		case 'k': caracter = 107; num = caracter; break;
-		case 'l': caracter = 108; num = caracter; break;
-		case 'm': caracter = 109; num = caracter; break;
-		case 'n': caracter = 110; num = caracter; break;
-		case 'o': caracter = 111; num = caracter; break;
-		case 'p': caracter = 112; num = caracter; break;
-		case 'q': caracter = 113; num = caracter; break;
-		case 'r': caracter = 114; num = caracter; break;
-		case 's': caracter = 115; num = caracter; break;
-		case 't': caracter = 116; num = caracter; break;
-		case 'u': caracter = 117; num = caracter; break;
-		case 'v': caracter = 118; num = caracter; break;
-		case 'w': caracter = 119; num = caracter; break;
-		case 'x': caracter = 120; num = caracter; break;
-		case 'y': caracter = 121; num = caracter; break;
-		case 'z': caracter = 122; num = caracter; break;
+	if (caracter == '\n'){
+		printf("\nEscreva o texto que deseja converter em número binário:\n");
+		dec_for_bin();
 	}
 	return num;
 }
@@ -91,14 +66,13 @@ void dec_for_bin(){
 	for (int i = 0; i < 8; i++){
 		printf("%i", bin[i]);
 	}
-	printf("\n\n");
+	printf("  ");
+
 	return dec_for_bin();
 }
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	/* code */	
-	dec_for_bin();
-
-	return 0;
+	menu();
 }
--- funcoes.h
+++ funcoes.h
@@ -3,3 +3,5 @@ int char_for_int();
 void dec_for_bin();
 
 int main(); 
+
+void menu();
